c21a7be791bcf7ccffbde335e1cd6c1e3203b858
AJEET69 <ajeetsinghverma69@gmail.com>
2025-05-26 12:50:01 +0530

    All the files 20 Sept

---
+++ src/Sept/ex_20_09_2024/Lab056.java
@@ -0,0 +1,13 @@
+package Sept.ex_20_09_2024;
+
+public class Lab056 {
+    public static void main(String[] args) {
+        // Ternary operator
+        // condition ? expression _if_ture : expression_if_false
+     int tushar = 4;
+     int prabhu = 7;
+     String result = tushar > prabhu ? "Tushar won" : "Prabhu won";
+        System.out.println(result);
+
+    }
+}
---
+++ src/Sept/ex_20_09_2024/Lab063.java
@@ -0,0 +1,13 @@
+package Sept.ex_20_09_2024;
+
+public class Lab063 {
+    public static void main(String[] args) {
+        boolean b = !true;
+        if(b){
+        System.out.println("b is true");
+        }else {
+        System.out.println("b is not true");
+    }
+
+    }
+}
